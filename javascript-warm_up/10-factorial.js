9533cb9623d6a6b8b56923f687560ff4ca2ccc79
David ROSET <snaboy18@gmail.com>
2026-03-20 10:00:33 +0000

    JavaScript - Warm up

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,10 @@
+#!/usr/bin/node
 function factorial(n) {
-	if (Number.isNaN(n) || n <= 1) {
-		return 1;
-	}
+  if (Number.isNaN(n) || n <= 1) {
+    return 1;
+  }
 
-	return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(Number.parseInt(process.argv[2])));
--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
+#!/usr/bin/node
 const parsedNumber = Number.parseInt(process.argv[2]);
 
 if (Number.isNaN(parsedNumber)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + parsedNumber);
+  console.log('My number: ' + parsedNumber);
 }
